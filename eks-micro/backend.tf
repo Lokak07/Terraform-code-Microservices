1476fe6ffd53d2822869f0d571febaaf129d4cb2
Shakthi <63875574+Lokak07@users.noreply.github.com>
2024-04-26 19:14:47 +0530

    Update backend.tf

--- eks-micro/backend.tf
+++ eks-micro/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "Shakthi-MANJUNATHAN"  # Update with your organization name
-    token        = var.TF_TOKEN
+    token        = env.TF_TOKEN
 
     workspaces {
       name = "microservices"
